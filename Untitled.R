34eb7ed8ed28cc15104439262766d422dd00eab5
Renata Diaz <diaz.renatam@gmail.com>
2019-06-20 17:09:06 -0400

    playing with methods in Downing et al 2014

--- Untitled.R
+++
@@ -1,8 +0,0 @@
-
-
-collection <- ggplot2::ggplot(data = toy_df, ggplot2::aes(x = quantile_range, y = nbind)) +
-#  ggplot2::geom_point(data = toy_df, ggplot2::aes(x = quantile_range, y = nbind), color = "red") + 
-  ggplot2::geom_point(data = toy_df, ggplot2::aes(x = quantile_range, y = nbspp), color = "blue") +
-  ggplot2::geom_point(data = toy_df, ggplot2::aes(x = quantile_range, y = spp_per_ind), color = "purple")
-
-collection
