70492411aba752513074506d3f5097ec5610ebfe
Renata Diaz <diaz.renatam@gmail.com>
2019-06-26 15:40:16 -0400

    applied to all erenst datasets

--- functions/integrated_density.R
+++ functions/integrated_density.R
@@ -76,7 +76,7 @@ find_pit_sections <- function(pit, integrated_density, threshold) {
 find_all_pits <- function(integrated_density, threshold = 0.05) {
   
   pits <- integrated_density %>%
-    dplyr::filter(start_is_pit) 
+    dplyr::filter(start_is_pit, by_max <= threshold) 
   
   if(!any(pits$by_max <= threshold)) {
     return(NULL)
@@ -93,7 +93,7 @@ find_all_pit_boundaries <- function(integrated_density, threshold = 0.05) {
                             threshold = threshold) 
   if(is.null(all_pits)) {
     warning("No troughs below threshold")
-    return(integrated_density)
+    return(NULL)
   }
   
   pit_boundaries <- data.frame(
@@ -114,6 +114,12 @@ find_all_pit_boundaries <- function(integrated_density, threshold = 0.05) {
 add_all_pit_boundaries <- function(integrated_density, threshold = 0.05) {
   pit_boundaries <- find_all_pit_boundaries(integrated_density = integrated_density,
                                             threshold = threshold)
+  if(is.null(pit_boundaries)) {
+    integrated_density$start_is_trough_start <- FALSE
+    integrated_density$start_is_trough_stop <- FALSE
+    integrated_density$is_in_trough <- FALSE
+    return(integrated_density)
+  }
   
   integrated_density$start_is_trough_start <- integrated_density$start %in% pit_boundaries$pit_start
   integrated_density$start_is_trough_stop <- integrated_density$start %in% pit_boundaries$pit_stop
@@ -125,4 +131,130 @@ add_all_pit_boundaries <- function(integrated_density, threshold = 0.05) {
   }
   
   return(integrated_density)
+}
+
+get_dat_p <- function(dat_p_args = list(integrated_density, isd)) {
+  
+  integrated_density <- dat_p_args$integrated_density
+  dat_isd <- dat_p_args$isd
+  
+  intd_tojoin <- integrated_density %>%
+    dplyr::rename(ln_size2 = start) %>%
+    dplyr::mutate(ln_size2 = as.character(ln_size2))
+  
+  dat_p <- dat_isd %>%
+    dplyr::mutate(ln_size2 = as.character(signif(ln_size, digits = 3))) %>%
+    dplyr::left_join(intd_tojoin, by = "ln_size2") %>%
+    dplyr::select(-ln_size2)
+  
+  return(dat_p)
+}
+
+
+get_species_summaries <- function(dat_p) {
+  
+  species_summaries <- dat_p %>%
+    dplyr::group_by(individual_species_ids) %>%
+    dplyr::summarise(mean_size = mean(ln_size),
+                     mean_p = mean(by_max),
+                     sd_p = sd(by_max),
+                     prop_in_trough = mean(is_in_trough),
+                     n_ind = dplyr::n()) %>%
+    dplyr::ungroup()
+  species_summaries <- species_summaries %>%
+    dplyr::mutate(sd_p = tidyr::replace_na(data = species_summaries$sd_p, replace = 0))
+  return(species_summaries)
+}
+
+plot_individuals <- function(dat_p, species_summary) {
+  
+  individuals_plot <- ggplot2::ggplot(data = dat_p, ggplot2::aes(x = ln_size, y = by_max, color = individual_species_ids)) + 
+    ggplot2::geom_jitter(size = .5) +
+    ggplot2::geom_point(data = species_summary, ggplot2::aes(x = mean_size, y = mean_p, color = individual_species_ids), size = 3) +
+    ggplot2::geom_hline(yintercept = 0.05) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.position = "none")
+  
+  return(individuals_plot) 
+}
+
+
+get_trough_individuals <- function(integrated_density, dat_isd, species_summary) {
+  
+  trough_sizes <- integrated_density %>%
+    dplyr::filter(is_in_trough) %>%
+    dplyr::select(start) %>%
+    dplyr::mutate(ln_size = signif(start, digits = 4)) %>%
+    dplyr::select(ln_size) %>%
+    dplyr::distinct()
+  
+  trough_ind <- dat_isd %>%
+    dplyr::mutate(ln_size = signif(ln_size, digits = 4)) %>%
+    dplyr::filter(ln_size %in% trough_sizes$ln_size) %>%
+    dplyr::left_join(species_summary, by = "individual_species_ids") %>%
+    dplyr::select(-individual_sizes, -individual_energy, -size_class, -size_class_g, -ln_energy) %>%
+    dplyr::arrange(dplyr::desc(individual_species_ids)) %>%
+    dplyr::mutate(relative_size = ln_size/mean_size)
+  
+  return(trough_ind)
+  
+}
+
+
+plot_trough_individuals <- function(trough_ind) {
+  
+  if(nrow(trough_ind) == 0) {
+    trough_plot <- ggplot2::ggplot(data = trough_ind, ggplot2::aes(x = prop_in_trough, y = relative_size, color = individual_species_ids, size = n_ind))
+    return(trough_plot)
+    }
+  
+  max_rel_size = max(trough_ind$relative_size, na.rm =T) + 1
+  
+  trough_plot <- ggplot2::ggplot(data = trough_ind, ggplot2::aes(x = prop_in_trough, y = relative_size, color = individual_species_ids)) +
+    ggplot2::xlim(0, 1) + 
+    ggplot2::ylim(0, max_rel_size) +
+    ggplot2::geom_hline(yintercept = 1) +
+    ggplot2::geom_jitter() +
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.position = "none")
+  
+  return(trough_plot)
+}
+
+
+get_mode_dom <- function(threshold, dat_p) {
+  
+  N <- as.numeric(nrow(dat_p))
+  S <- as.numeric(length(unique(dat_p$individual_species_ids)))
+  
+  modes <- dat_p %>%
+    dplyr::filter(by_max > threshold)
+  
+  mode_richness <- as.numeric(length(unique(modes$individual_species_ids)))
+  mode_abund <- as.numeric(nrow(modes))
+  
+  mode_dom <- (mode_richness / S) / (mode_abund / N)
+  
+  return(mode_dom)
+}
+
+get_mode_thresholded <- function(dat_p) {
+  mode_thresholded <- data.frame(
+    threshold = seq(.01, 1, by = 0.01),
+    mode_dom = vapply(seq(0.01, 1, by = 0.01), FUN = get_mode_dom, 
+                      dat_p = dat_p, FUN.VALUE = .75)
+  )
+  return(mode_thresholded)
+}
+
+plot_mode_dom <- function(mode_thresholded) {
+  
+  thresh_plot <- ggplot2::ggplot(data = mode_thresholded, 
+                                 ggplot2::aes(x = threshold, y = mode_dom)) +
+    ggplot2::geom_point() + 
+    ggplot2::geom_hline(yintercept = 1) +
+    ggplot2::theme_bw()
+  
+  return(thresh_plot)
+  
 }
