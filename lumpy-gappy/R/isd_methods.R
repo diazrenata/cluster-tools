60bc35074b7f6510a5c00e70965ada17014dbaa8
Renata Diaz <diaz.renatam@gmail.com>
2019-06-27 17:13:05 -0400

    Add sim at scale functionality. Note odd behavior in sim 5

--- lumpy-gappy/R/isd_methods.R
+++ lumpy-gappy/R/isd_methods.R
@@ -9,9 +9,140 @@ make_isd <- neonbecs::make_isd
 fit_gmm <- neonbecs::fit_gmm
 
 # Integrate density of GMM
+get_integrated_density <- function(gmm, interval_size = 0.0001, min_size = 0, max_size = 8) {
+  
+  density_evalpoints <- seq(min_size, max_size, by = interval_size)
+  
+  density_estimates <- predict(gmm, newdata = density_evalpoints)
+  
+  integrated_density <- data.frame(start = density_evalpoints[1:length(density_evalpoints) -1],
+                                   stop = density_evalpoints[2:length(density_evalpoints)],
+                                   start_density = density_estimates[1:length(density_evalpoints) -1]) %>%
+    dplyr::mutate(density = start_density * (stop - start),
+                  by_max = density / max(density))
+  
+  p_turnpoints <- pastecs::turnpoints(integrated_density$by_max)
+  
+  integrated_density$start_is_peak <- p_turnpoints$peaks[1:length(density_evalpoints) -1 ]
+  integrated_density$start_is_pit <- p_turnpoints$pits[1:length(density_evalpoints) -1 ]
+  integrated_density$start_is_turnpoint <- (integrated_density$start_is_peak | integrated_density$start_is_pit)
+  
+  return(integrated_density)
+}
 
-# Find maxima and minima in GMM
 
 # Find troughs in density to a given threshold
+find_pit_sections <- function(pit, integrated_density, threshold) {
+  
+  turns <- integrated_density %>%
+    dplyr::filter(start_is_turnpoint) %>%
+    dplyr::mutate(start_is_peak = by_max >= threshold)
+  
+  lower_peak <- turns %>%
+    dplyr::filter(start_is_peak, start <= pit) %>%
+    dplyr::filter(start == max(start))
+  
+  upper_peak <- turns %>%
+    dplyr::filter(start_is_peak, start >= pit) %>%
+    dplyr::filter(start == min(start))
+  
+  pit_subset <- integrated_density %>%
+    dplyr::filter(dplyr::between(start, lower_peak$start[1], upper_peak$start[1]))
+  
+  pit_below_threshold <- pit_subset %>%
+    dplyr::filter(by_max <= threshold)
+  
+  return(pit_below_threshold)
+}
+
+find_all_pits <- function(integrated_density, threshold = 0.05) {
+  
+  pits <- integrated_density %>%
+    dplyr::filter(start_is_pit, by_max <= threshold) 
+  
+  if(!any(pits$by_max <= threshold)) {
+    return(NULL)
+  }
+  
+  all_pits <- lapply(pits$start, FUN = find_pit_sections, 
+                     integrated_density = integrated_density,
+                     threshold = threshold)
+  return(all_pits)
+}
+
+find_all_pit_boundaries <- function(integrated_density, threshold = 0.05) {
+  all_pits <- find_all_pits(integrated_density = integrated_density,
+                            threshold = threshold) 
+  if(is.null(all_pits)) {
+    warning("No troughs below threshold")
+    return(NULL)
+  }
+  
+  pit_boundaries <- data.frame(
+    pit_index = 1:length(all_pits),
+    pit_start = NA,
+    pit_stop = NA,
+    pit_length = NA
+  )
+  
+  for(i in 1:nrow(pit_boundaries)) {
+    pit_boundaries$pit_start[i] <- min(all_pits[[i]]$start)
+    pit_boundaries$pit_stop[i] <- max(all_pits[[i]]$stop)
+    pit_boundaries$pit_length[i] <- pit_boundaries$pit_stop[i] - pit_boundaries$pit_start[i]
+  }
+  return(pit_boundaries) 
+}
+
+add_all_pit_boundaries <- function(integrated_density, threshold = 0.05) {
+  peaks_above_thresh <- dplyr::filter(integrated_density,
+                                      start_is_peak == TRUE,
+                                      by_max >= threshold)
+  
+  if(nrow(peaks_above_thresh) < 2) {
+    integrated_density$start_is_trough_start <- FALSE
+    integrated_density$start_is_trough_stop <- FALSE
+    integrated_density$is_in_trough <- FALSE
+    warning("Fewer than 2 peaks above threshold")
+    return(integrated_density)
+  }
+  pit_boundaries <- find_all_pit_boundaries(integrated_density = integrated_density,
+                                            threshold = threshold)
+  if(is.null(pit_boundaries)) {
+    integrated_density$start_is_trough_start <- FALSE
+    integrated_density$start_is_trough_stop <- FALSE
+    integrated_density$is_in_trough <- FALSE
+    return(integrated_density)
+  }
+  
+  integrated_density$start_is_trough_start <- integrated_density$start %in% pit_boundaries$pit_start
+  integrated_density$start_is_trough_stop <- integrated_density$start %in% pit_boundaries$pit_stop
+  integrated_density$is_in_trough <- FALSE
+  for(i in 1:nrow(pit_boundaries)) {
+    integrated_density$is_in_trough[ which(dplyr::between(integrated_density$start, 
+                                                          pit_boundaries$pit_start[i],
+                                                          pit_boundaries$pit_stop[i]))] <- TRUE
+  }
+  
+  return(integrated_density)
+}
 
 # Count modes and troughs in density
+
+isd_methods_summary <- function(integrated_density, threshold) {
+  nmodes  <- sum(integrated_density$start_is_peak)
+  
+  nmodes_above_threshold <- nrow(dplyr::filter(integrated_density,
+                           start_is_peak == TRUE,
+                           by_max >= threshold))
+  
+  ntroughs <- sum(integrated_density$start_is_trough_start)
+  
+  ntroughs_possible <- nmodes_above_threshold - 1
+  
+  summary_variables <- list(nmodes = nmodes, 
+                         nmodes_above_threshold = nmodes_above_threshold,
+                         ntroughs = ntroughs,
+                         ntroughs_possible = ntroughs_possible)
+  
+  return(summary_variables)
+}
--- lumpy-gappy/R/plot_methods.R
+++ lumpy-gappy/R/plot_methods.R
@@ -3,8 +3,36 @@
 plot_isd <- neonbecs::plot_isd
 
 # Plot integrated density
+
 ## Options:
 ### With maxima/minima
 ### With threshold lines
 ### With trough boundaries demarcated
 
+plot_integrated_density <- function(integrated_density, threshold_lines = NULL, pit_boundaries = FALSE) {
+  
+  integrated_plot <- ggplot2::ggplot(data = integrated_density, ggplot2::aes(x = start, y = by_max)) +
+    ggplot2::geom_point(inherit.aes = TRUE, size = .1) +
+    ggplot2::geom_point(data = integrated_density[ which(integrated_density$start_is_peak), c("start", "by_max")], inherit.aes = TRUE, color = "green", size = 2) +
+    ggplot2::geom_point(data = integrated_density[ which(integrated_density$start_is_pit), c("start", "by_max")], inherit.aes = TRUE, color = "blue", size = 2) +
+    ggplot2::theme_bw()
+  
+  if(!is.null(threshold_lines)) {
+    integrated_plot <- integrated_plot +
+      ggplot2::geom_hline(yintercept = threshold_lines, color = "red")
+  }
+  
+  if(pit_boundaries) {
+    
+    if(!("start_is_trough_start" %in% colnames(integrated_density))) {
+      warning("Missing trough boundaries")
+      return(integrated_plot)
+    }
+    
+    integrated_plot <- integrated_plot + 
+      ggplot2::geom_point(data = integrated_density[ which(integrated_density$start_is_trough_start), c("start", "by_max")], inherit.aes = TRUE, color = "red", size = 2, shape = 4) +
+      ggplot2::geom_point(data = integrated_density[ which(integrated_density$start_is_trough_stop), c("start", "by_max")], inherit.aes = TRUE, color = "red", size = 2, shape = 4) 
+  }
+  
+  return(integrated_plot)
+}
